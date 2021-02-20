ace0d15632078de2648b002ac42094f305971c10
hashcodepk <hashirlives@gmail.com>
2021-02-21 02:25:51 +0500

    .

---
+++ script.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  let mydata = [
+    { first_name: "Waqas", last_name: "Mehmood" },
+    { first_name: "Faraz", last_name: "Mehmood" },
+    { first_name: "Hashir", last_name: "Ghouri" },
+  ];
+
+  $.each(mydata, function (i, val) {
+    $("#tableBody").append(`<tr>
+      <td>${val.first_name}</td>
+      <td>${val.last_name}</td>
+    </tr>`);
+  });
+
+  //adding data with form
+
+  $("#myForm").submit(function (e) {
+    e.preventDefault();
+
+    let firstName = $("input#firstName").val();
+    let lastName = $("input#lastName").val();
+
+    $("#tableBody").append(`<tr>
+    <td>${firstName}</td>
+    <td>${lastName}</td>
+    </tr>`);
+
+    $("input#firstName").val("");
+    $("input#lastName").val("");
+  });
+});
