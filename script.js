84ce8543ce6f0e2a65e99e6d500e153b93375ddd
hashcodepk <hashirlives@gmail.com>
2021-02-21 16:41:24 +0500

    Completed All Tasks

--- script.js
+++ script.js
@@ -1,38 +1,93 @@
+//Dummy Data
+
+let jsonData = [
+  {
+    id: "1",
+    firstName: "Waqas",
+    lastName: "Mehmood",
+  },
+  {
+    id: "2",
+    firstName: "Faraz",
+    lastName: "Mehmood",
+  },
+  {
+    id: "4",
+    firstName: "George",
+    lastName: "Bluth",
+  },
+  {
+    id: "5",
+    firstName: "Janet",
+    lastName: "Weaver",
+  },
+  {
+    id: "6",
+    firstName: "Emma",
+    lastName: "Wong",
+  },
+  {
+    id: "7",
+    firstName: "Charles",
+    lastName: "Morris",
+  },
+];
+
+// SaveData Function
+
+const saveData = function () {
+  let firstName = $("input#firstName");
+  let lastName = $("input#lastName");
+  if (firstName && lastName) {
+    alert(firstName.val() + " " + lastName.val() + " Saved!");
+    firstName.val("");
+    lastName.val("");
+  } else {
+    alert("Please fill the form");
+  }
+};
+
+// getData Function
+
+const getData = function (data) {
+  return data;
+};
+
+// renderData Function
+
+let dataCols = [{ data: "firstName" }, { data: "lastName" }];
+
+const renderData = function () {
+  $("#myTable").DataTable({
+    data: getData(jsonData),
+    columns: dataCols,
+  });
+};
+
+// function to populate form
+
+$.fn.fillForm = function (data) {
+  $("input#firstName").val(data.firstName);
+  $("input#lastName").val(data.lastName);
+};
+
 $(document).ready(function () {
-  // let mydata = [
-  //   { first_name: "Waqas", last_name: "Mehmood" },
-  //   { first_name: "Faraz", last_name: "Mehmood" },
-  //   { first_name: "Hashir", last_name: "Ghouri" },
-  // ];
+  renderData();
 
-  // $.each(mydata, function (i, val) {
-  //   $("#tableBody").append(`<tr>
-  //     <td>${val.first_name}</td>
-  //     <td>${val.last_name}</td>
-  //   </tr>`);
-  // });
+  $("#btnSubmit").on("click", () => saveData());
 
-  //adding data with form
+  let table = $("#myTable").DataTable();
+  let resetBtn = $("button#btnReset");
 
-  $("#myForm").submit(function (e) {
-    e.preventDefault();
-
-    let firstName = $("input#firstName").val();
-    let lastName = $("input#lastName").val();
-
-    $("#tableBody").append(`<tr>
-    <td>${firstName}</td>
-    <td>${lastName}</td>
-    </tr>`);
-
-    $("input#firstName").val("");
-    $("input#lastName").val("");
+  $("#myTable tbody").on("click", "tr", function () {
+    let clickedData = table.row(this).data();
+    $.fn.fillForm(clickedData);
+    resetBtn.show();
   });
 
-  //using DataTables Plugin
-
-  $("#myTable").DataTable({
-    ajax: "data.js",
-    columns: [{ data: "firstName" }, { data: "lastName" }],
+  resetBtn.on("click", function () {
+    $("input#firstName").val("");
+    $("input#lastName").val("");
+    resetBtn.hide();
   });
 });
